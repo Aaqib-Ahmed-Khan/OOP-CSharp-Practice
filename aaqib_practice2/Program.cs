97f3a815ed61df5319c5cb1ffad35545f9c02f54
Aaqib-Ahmed-Khan <155422396+Aaqib-Ahmed-Khan@users.noreply.github.com>
2025-04-04 00:37:55 +0500

    Initial commit

---
+++ aaqib_practice2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace aaqib_practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //String fullName = "aaqib ahmed";
+            //String phoneNumber = "12 345 6";
+            //fullname=fullname.ToUpper();
+            //fullname = fullname.ToLower()
+            //;            Console.WriteLine(fullname);
+            //phoneNumber = phoneNumber.Replace(" ", "-");
+            //Console.WriteLine(phoneNumber);
+            //String username = fullname.Insert(11, " khan");
+            //Console.WriteLine(username);
+
+            //Console.WriteLine(fullName.Length);
+            //Console.WriteLine("enter your age");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //if(age>=18)
+            //{
+            //    Console.WriteLine("you are eligible to vote");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("you are inelegible to vote");
+            //}
+            string name = "aaqib";
+            greet(name);
+            greet(name);
+            Console.ReadKey();
+
+        }
+        static void greet(String name)
+        {
+            Console.WriteLine("hey " + name + " how are you");
+        }
+    }
+}
